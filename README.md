2ba27b668ce7cdbb9de31376451fbb67f063d224
Trung Dang <trungdt@absoft.vn>
2017-06-02 15:19:11 +0800

    update readme

--- README.md
+++ README.md
@@ -7,3 +7,16 @@
 [![Download Status](https://img.shields.io/npm/immanuel192/seneca-utils.svg?style=flat-square)](https://www.npmjs.com/package/seneca-utils)
 
 > Seneca Service Loader to help you easily buiild seneca service
+
+## Change logs
+### v1.0.3
+- Add Dto support. Now when exporting your command, add dto type and subtype
+```javascript
+module.exports = {
+    pin: 'cmd:test',
+    Func: CommandTest,
+    name: 'test',
+    dtoType: '', // your dto main type
+    dtoSubType: '' // your dto sub type
+};
+```
--- SenecaLoaderService.js
+++ SenecaLoaderService.js
@@ -42,7 +42,7 @@ class SenecaLoaderService {
         }
         else if (args.length === 3 && typeof args[1] === 'function') {
             const serviceInstance = new args[1]();
-            this.seneca.add(args[0], CommandWrapper(args[2], serviceInstance.func.bind(serviceInstance), serviceInstance.dto));
+            this.seneca.add(args[0], CommandWrapper(args[2], serviceInstance.func.bind(serviceInstance), serviceInstance.dtoType, serviceInstance.dtoSubType));
             this.logger.info(`Loaded command ${args[2]}`);
 
             return true;
